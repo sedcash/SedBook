5255a902394010ea8851571c63521814b8738f25
Sedrick Cashaw Jr <sedrickcashawjr@Sedricks-MacBook-Air.local>
2017-09-15 13:21:32 -0400

    Bookmark Images

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sedrickcashawjr/Documents/Projects/Github/Web/Back_End/Python/Django/SedBook/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
